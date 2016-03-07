558011d2a9f0462c39f77f04cdccfb713cb2f985
Korbinian Hörfurter <korbi_@web.de>
2016-03-07 17:46:14 +0100

    Restructure

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./lib/Viewer');
+module.exports = {
+    __init__: [ 'contextPadProvider','aofActionListener','aofPaletteProvider' ,'replaceMenuProvider','customRules','appManager'],
+    contextPadProvider: [ 'type', require('./lib/aof-context-pad') ],
+    aofActionListener: [ 'type', require('./lib/aof-action-listener')],
+    aofPaletteProvider: [ 'type', require('./lib/aof-palette-provider') ],
+    replaceMenuProvider: [ 'type', require('./lib/aof-popup-menu') ],
+    customRules: [ 'type', require('./lib/aof-rules') ],
+    appManager: [ 'type', require('./lib/app-manager') ]
+};
