b4193529386dd923f9db48d3fd14d1c770f1ecb9
Korbinian Hörfurter <korbi_@web.de>
2016-03-07 18:31:06 +0100

    Included all required modules

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = {
-    __init__: [ 'contextPadProvider','aofActionListener','aofPaletteProvider' ,'replaceMenuProvider','customRules','appManager'],
+    __init__: [ 'contextPadProvider','aofActionListener','aofPaletteProvider' ,'replaceMenuProvider','customRules','appManager','propertiesPanel'],
     contextPadProvider: [ 'type', require('./lib/aof-context-pad') ],
     aofActionListener: [ 'type', require('./lib/aof-action-listener')],
     aofPaletteProvider: [ 'type', require('./lib/aof-palette-provider') ],
     replaceMenuProvider: [ 'type', require('./lib/aof-popup-menu') ],
     customRules: [ 'type', require('./lib/aof-rules') ],
-    appManager: [ 'type', require('./lib/app-manager') ]
+    appManager: [ 'type', require('./lib/app-manager') ],
+    propertiesPanel: [ 'type', require('bpmn-js-properties-panel/lib/PropertiesPanel') ],
+    propertiesProvider: [ 'type', require('bpmn-js-properties-panel/lib/provider/aof/AofPropertiesProvider') ]
 };
