7f16d1879536da87eb63c9ae8dc830437a9ea72f
Korbinian Hörfurter <korbi_@web.de>
2016-03-08 13:37:17 +0100

    Renamed customRules into aofRules

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = {
-    __init__: [ 'contextPadProvider','aofActionListener','aofPaletteProvider' ,'replaceMenuProvider','customRules','appManager','propertiesPanel'],
+    __init__: [ 'contextPadProvider','aofActionListener','aofPaletteProvider' ,'replaceMenuProvider','aofRules','appManager','propertiesPanel'],
     contextPadProvider: [ 'type', require('./lib/aof-context-pad') ],
     aofActionListener: [ 'type', require('./lib/aof-action-listener')],
     aofPaletteProvider: [ 'type', require('./lib/aof-palette-provider') ],
     replaceMenuProvider: [ 'type', require('./lib/aof-popup-menu') ],
-    customRules: [ 'type', require('./lib/aof-rules') ],
+    aofRules: [ 'type', require('./lib/aof-rules') ],
     appManager: [ 'type', require('./lib/app-manager') ],
     propertiesPanel: [ 'type', require('bpmn-js-properties-panel/lib/PropertiesPanel') ],
     propertiesProvider: [ 'type', require('bpmn-js-properties-panel/lib/provider/aof/AofPropertiesProvider') ]
